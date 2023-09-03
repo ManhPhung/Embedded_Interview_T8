d61840fa864d9682655a917e041585881500e66b
Phung Van Manh <phungmanh1620@gmail.com>
2023-09-03 15:12:24 +0700

    ok

--- Home_Work/BT_1_9_23/bai2.c
+++ Home_Work/BT_1_9_23/bai2.c
@@ -12,26 +12,30 @@ typedef enum{
     T7
 }dayOfWeek;
 
+typedef struct{
+    int day, month, year;
+}dayInput;
 
 int main() {
-    int day, month, year;
+    
+    dayInput dayIn;
 
     // Nhập ngày, tháng và năm từ người dùng
     printf("Nhap ngay (1-31): ");
-    scanf("%d", &day);
+    scanf("%d", &dayIn.day);
     printf("Nhap thang (1-12): ");
-    scanf("%d", &month);
+    scanf("%d", &dayIn.month);
     printf("Nhap nam: ");
-    scanf("%d", &year);
+    scanf("%d", &dayIn.year);
 
     // Kiểm tra xem ngày, tháng và năm có hợp lệ không
-    if (day < 1 || day > 31 || month < 1 || month > 12) {
+    if (dayIn.day < 1 || dayIn.day > 31 || dayIn.month < 1 || dayIn.month > 12) {
         printf("Ngay thang khong hop le.\n");
         return 1;
     }
 
     // Kiểm tra xem tháng 2 có bao nhiêu ngày
-    int isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+    int isLeapYear = (dayIn.year % 4 == 0 && (dayIn.year % 100 != 0 || dayIn.year % 400 == 0));
     int daysInFebruary = (isLeapYear) ? 29 : 28;
 
     // Mảng lưu số ngày trong mỗi tháng
@@ -39,7 +43,7 @@ int main() {
 
     // Tính tổng số ngày tính từ 1/1/1 đến ngày nhập vào
     int totalDays = 0;
-    for (int i = 1; i < year; i++) {
+    for (int i = 1; i < dayIn.year; i++) {
         if (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
             totalDays += 366; // Năm nhuận
         } else {
@@ -47,11 +51,11 @@ int main() {
         }
     }
 
-    for (int i = 1; i < month; i++) {
+    for (int i = 1; i < dayIn.month; i++) {
         totalDays += daysInMonth[i]; // Cộng thêm số ngày trong các tháng trước đó
     }
 
-    totalDays += day; // Cộng thêm số ngày của tháng hiện tại
+    totalDays += dayIn.day; // Cộng thêm số ngày của tháng hiện tại
 
     // Tìm thứ của ngày nhập vào (0: Chủ Nhật, 1: Thứ Hai, 2: Thứ Ba, ..., 6: Thứ Bảy)
     int dayOfWeek = totalDays % 7;
@@ -59,25 +63,25 @@ int main() {
     switch (dayOfWeek)
     {
     case T2:
-        printf("Ngay %d/%d/%d la thu hai.\n", day, month, year);
+        printf("Ngay %d/%d/%d la thu hai.\n", dayIn.day, dayIn.month, dayIn.year);
         break;
     case T3:
-        printf("Ngay %d/%d/%d la thu ba.\n", day, month, year);
+        printf("Ngay %d/%d/%d la thu ba.\n", dayIn.day, dayIn.month, dayIn.year);
         break;
     case T4:
-        printf("Ngay %d/%d/%d la thu tu.\n", day, month, year);
+        printf("Ngay %d/%d/%d la thu tu.\n", dayIn.day, dayIn.month, dayIn.year);
         break;
     case T5:
-        printf("Ngày %d/%d/%d là thứ năm.\n", day, month, year);
+        printf("Ngày %d/%d/%d là thứ năm.\n", dayIn.day, dayIn.month, dayIn.year);
         break;
     case T6:
-        printf("Ngay %d/%d/%d la thu sau.\n", day, month, year);
+        printf("Ngay %d/%d/%d la thu sau.\n", dayIn.day, dayIn.month, dayIn.year);
         break;
     case T7:
-        printf("Ngay %d/%d/%d la thu bay.\n", day, month, year);
+        printf("Ngay %d/%d/%d la thu bay.\n", dayIn.day, dayIn.month, dayIn.year);
         break;
     case CN:
-        printf("Ngay %d/%d/%d la chu nhat.\n", day, month, year);
+        printf("Ngay %d/%d/%d la chu nhat.\n", dayIn.day, dayIn.month, dayIn.year);
         break;    
     }
 
